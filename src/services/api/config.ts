3baa33ca5dab93cb54f0caf27a2a1e17d05e00f2
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2021-03-24 13:07:18 +0100

    Use proxy instead of axios baseURL

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,9 +1,5 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
-const apiConfig: AxiosRequestConfig = {
-    baseURL: `${process.env.REACT_APP_API_BASE_URL}/${process.env.REACT_APP_API_VERSION}/`,
-    timeout: 3000,
-};
-const Api = axios.create(apiConfig);
+const Api = axios.create();
 
 export default Api;
